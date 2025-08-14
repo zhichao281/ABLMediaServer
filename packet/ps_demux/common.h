c909ae48448956ed869d63c8fd356d1134c90727
zhichao281@163.com <a6808131>
2025-08-14 11:44:18 +0800

    +修改了冲突

--- packet/ps_demux/common.h
+++ packet/ps_demux/common.h
@@ -1,10 +1,7 @@
-#ifndef _PS_MUX_DEMUX_COMMON_H_
-#define _PS_MUX_DEMUX_COMMON_H_
 
+#pragma once
 #include <stdint.h>
+#include <cstring>
+uint32_t generate_identifier_psdemux();
 
-uint32_t generate_identifier();
-
-void recycle_identifier(uint32_t id);
-
-#endif 
+void recycle_identifier_psdemux(uint32_t id);
